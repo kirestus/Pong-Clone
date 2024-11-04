d8a1096aa03b6c549a39484dbea9e2d466f4a326
Frylock <johnryanfry88@gmail.com>
2024-11-03 21:34:41 -0800

    getting it working in vscode

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include <SFML/Graphics.hpp>
 
+int main();
+
 int main()
 {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(1280, 729), "PongClone");
+    sf::CircleShape shape(300.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event(); window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
