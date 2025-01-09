9490dc34bc6d8857ee6a3e02c6c2d3e6aaa98334
Frylock <johnryanfry88@gmail.com>
2025-01-08 22:20:13 -0800

    Changed some of my CMAKE build settings and got the font working by using a direct path to the source

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,22 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "PongClone");
-    sf::Vector2f screenCetre = (sf::Vector2(float(window.getSize().x/2),float(window.getSize().y/2)));
+    sf::Vector2f screenCetre = sf::Vector2f(window.getSize().x/2, window.getSize().y/2);
     sf::CircleShape shape(300.f);
 
-    shape.setFillColor(sf::Color::White);
+    shape.setFillColor(sf::Color::Blue);
     shape.setOrigin(shape.getRadius(),shape.getRadius());
     shape.setPosition(screenCetre);
 
     sf::Font vector;
-    vector.loadFromFile("./fonts/Vector.ttf");
+    vector.loadFromFile("/Users/johnfry/SFML_Projects/Pong-Clone/src/fonts/Vector.ttf");
 
     sf::Text messageText;
     messageText.setFont(vector);
-    messageText.setString("pong");
-    messageText.setCharacterSize(30);
-    messageText.setFillColor(sf::Color::White);
-
+    messageText.setString("PonG");
+    messageText.setCharacterSize(56);
+    messageText.setFillColor(sf::Color::Green);
+    messageText.setPosition(screenCetre.x,screenCetre.y);
 
     while (window.isOpen())
     {
